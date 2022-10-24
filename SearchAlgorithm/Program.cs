7bb94ad6e329153370a9bdf125cbd5ee7c346324
Adityo Arya Putra <adityo.arya.ft21@mail.umy.ac.id>
2022-10-24 17:03:48 +0700

    Accept the Number to be Searched

--- SearchAlgorithm/Program.cs
+++ SearchAlgorithm/Program.cs
@@ -87,8 +87,26 @@ namespace SearchAlgorithm
             int ctr;
             do
             {
+                //Accept the Number to be Searched
+                Console.Write("\nEnter the element you want to search: ");
+                int item = Convert.ToInt32(Console.ReadLine());
 
-            }
+                ctr = 0;
+                for (i = 0; i < n; i++)
+                {
+                    ctr++;
+                    if (arr[i] == item)
+                    {
+                        Console.WriteLine("\n" + item.ToString() + " found st position " + (i + 1).ToString());
+                        break;
+                    }
+                }
+                if (i == n)
+                    Console.WriteLine("\n" + item.ToString() + "not found in the array");
+                Console.WriteLine("\nNumber of Comparasion: " + ctr);
+                Console.Write("\nContinue Search (y/n): ");
+                ch = char.Parse(Console.ReadLine());
+            } while ((ch == 'y') || (ch == 'Y'));
         }
 
         static void Main(string[] args)
