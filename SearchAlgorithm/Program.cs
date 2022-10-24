c1e3843249bf30b3a135fa62421ae9208e1d361e
Adityo Arya Putra <adityo.arya.ft21@mail.umy.ac.id>
2022-10-24 16:38:28 +0700

    Loop to search for the elements in the array

--- SearchAlgorithm/Program.cs
+++ SearchAlgorithm/Program.cs
@@ -55,6 +55,18 @@ namespace SearchAlgorithm
                 //obtain The Index of the middle elements
                 int mid = (lowerbound + upperbound) / 2;
                 int ctr = 1;
+
+                //Loop to search for the elements in the array
+                while ((item != arr[mid]) && (lowerbound <= upperbound))
+                {
+                    if (item > arr[mid])
+                        lowerbound = mid + 1;
+                    else
+                        upperbound = mid - 1;
+
+                    mid = (lowerbound + upperbound) / 2;
+                    ctr++;
+                }
             }
         }
 
