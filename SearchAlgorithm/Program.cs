a34ace719bdad256de722c760f45907a7fdd6611
Adityo Arya Putra <adityo.arya.ft21@mail.umy.ac.id>
2022-10-24 16:19:12 +0700

    create while in public void input

--- SearchAlgorithm/Program.cs
+++ SearchAlgorithm/Program.cs
@@ -20,7 +20,13 @@ namespace SearchAlgorithm
         {
             while (true)
             {
-
+                Console.Write("Enter the Number of Elements in the array: ");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if ((n > 0) && (n <= 20))
+                    break;
+                else
+                    Console.WriteLine("\nArray should have minimum 1 and maximum 20 elements.\n");
             }
         }
 
